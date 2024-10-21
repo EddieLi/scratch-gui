33f7c6bc52ccce813e513e05763d50f76ea2750a
Eddie Li <lh.eddieli@gmail.com>
2024-10-21 00:29:20 -0400

    able to load file

--- src/playground/render-gui.jsx
+++ src/playground/render-gui.jsx
@@ -56,6 +56,18 @@ export default appTarget => {
         }
     }
 
+    const projectFileMatches = window.location.href.match(/[?&]project_file=([^&]*)&?/);
+    const projectFile = projectFileMatches ? decodeURIComponent(projectFileMatches[1]) : null;
+    const onVmInit = vm => {
+        if (projectFile) {
+            fetch(projectFile)
+                .then(response => response.arrayBuffer())
+                .then(arrayBuffer => {
+                    vm.loadProject(arrayBuffer);
+                });
+        }
+    };
+
     if (process.env.NODE_ENV === 'production' && typeof window === 'object') {
         // Warn before navigating away
         window.onbeforeunload = () => true;
@@ -80,6 +92,7 @@ export default appTarget => {
                 backpackHost={backpackHost}
                 canSave={false}
                 onClickLogo={onClickLogo}
+                onVmInit={onVmInit}
             />,
         appTarget);
 };
